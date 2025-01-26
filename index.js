8410706a1c16582004eaa76588682247d576a685
Nilesh Pataskar <nileshpataskars@gmail.com>
2025-01-26 14:21:22 +0530

    added cors

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ const bodyParser = require("body-parser");
 const connectDB = require("./db"); // MongoDB connection
 const authRoutes = require("./routes/auth"); // Authentication routes
 const userRoutes = require("./routes/user"); // User profile routes
+const cors = require("cors");
 
 const app = express();
+
+const corsOptions = {
+  origin: "*", // Allow all origins
+  methods: "*", // Allow only GET and POST methods (adjust if needed)
+  allowedHeaders: "Content-Type,Authorization", // Allow specific headers
+};
+
+app.use(cors(corsOptions));
 
 // Connect to MongoDB
 connectDB();
